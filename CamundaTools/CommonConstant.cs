0c75d0d5058ec7931c69c4020951829ac8fce020
Valeska Valentin Ekklesia <valeska.ve@ad-ins.com>
2026-03-30 15:36:24 +0700

    feat: finish retry all process

--- CamundaTools/CommonConstant.cs
+++ CamundaTools/CommonConstant.cs
@@ -9,5 +9,7 @@ namespace CamundaTools
     public class CommonConstant
     {
         public const string CAMUNDA_PROCESS_DEFINITION_ENDPOINT = "/engine-rest/process-instance?processDefinitionId=";
+        public const string CAMUNDA_PROCESS_GET_PROCESS_INSTANCE_ENDPOINT = "/engine-rest/job?processInstanceId=";
+        public const string CAMUNDA_PROCESS_RETRY_ENDPOINT = "/engine-rest/job/";
     }
 }
--- CamundaTools/Program.cs
+++ CamundaTools/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
@@ -19,18 +20,30 @@ namespace CamundaTools
         {
             Console.WriteLine("Welcome to Camunda Tools!!");
             Console.WriteLine("Developed by: Valeska Valentin Ekklesia");
-            string choice;
             Console.WriteLine("Please select an option:");
             Console.WriteLine("1. Retry All Process (activity based)");
-            choice = Console.ReadLine();
-            switch (choice)
+            Console.WriteLine("1. Retry All External Process (activity based)");
+            Console.WriteLine("3. Exit");
+            string choice = "12387281942";
+            while(choice != "3")
             {
-                case "1":
-                    RetryAllProcess().Wait();
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please select a valid option.");
-                    break;
+                Console.Write("Input >> ");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        RetryAllProcess().Wait();
+                        break;
+                    case "2":
+                        RetryAllProcess().Wait();
+                        break;
+                    case "3":
+                        Console.WriteLine("Exiting program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please select a valid option.");
+                        break;
+                }
             }
         }
 
@@ -51,7 +64,44 @@ namespace CamundaTools
                 }
                 string responseBody = await response.Content.ReadAsStringAsync();
                 var data = JsonSerializer.Deserialize<List<JsonElement>>(responseBody);
-                Console.WriteLine("Data available " + data.Count);
+                Console.WriteLine("Data available " + data.Count());
+
+                int dataSize = data.Count();
+                if (dataSize == 0)
+                {
+                    Console.WriteLine("No data process in this workflow");
+                    Console.ReadKey();
+                    return;
+                }
+                bool isFinished = false;
+                HttpResponseMessage jobResponse = new HttpResponseMessage();
+                Console.WriteLine("Processing...");
+                for (int i = 0; i < dataSize; i++)
+                {
+                    string jobProcessInstance = baseUrl + CommonConstant.CAMUNDA_PROCESS_GET_PROCESS_INSTANCE_ENDPOINT;
+                    jobProcessInstance += data[i].GetProperty("id").GetString();
+                    jobResponse = await httpClient.GetAsync(jobProcessInstance);
+                    if (!jobResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Failed to retrieve process instance. Status code: {response.StatusCode}");
+                        return;
+                    }
+                    string rawResponseJob = await jobResponse.Content.ReadAsStringAsync();
+                    var responseJob = JsonSerializer.Deserialize<List<JsonElement>>(rawResponseJob);
+                    string retryUrl = baseUrl + CommonConstant.CAMUNDA_PROCESS_RETRY_ENDPOINT + responseJob[0].GetProperty("id").GetString() + "/retries";
+                    var retryBody = new { retries = 1 };
+                    string retryJson = JsonSerializer.Serialize(retryBody);
+                    StringContent retryContent = new StringContent(retryJson, Encoding.UTF8, "application/json");
+                    HttpResponseMessage retryResponse = await httpClient.PutAsync(retryUrl, retryContent);
+
+                    if (i == dataSize - 1)
+                    {
+                        isFinished = true;
+                        Console.WriteLine("Process Complete!");
+                        Console.ReadKey();
+                    }
+
+                }
                 //Console.WriteLine(responseBody);
                 Console.ReadKey();
             }
